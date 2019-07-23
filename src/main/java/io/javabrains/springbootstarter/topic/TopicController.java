f8720c9dd6d754a1d799e78c3152b4f1224113c7
Kirill Dyakov <Dyakovks@gmail.com>
2019-07-23 21:21:13 +0300

    put + delete

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -15,20 +15,30 @@ public class TopicController {
 
 	@Autowired
 	private TopicService topicService;
-	
+
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 
 	}
-	
+
 	@RequestMapping("/topics/{id}")
 	public Topic getTopicById(@PathVariable String id) {
 		return topicService.getTopicById(id);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 	}
 }
